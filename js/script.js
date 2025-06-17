0c78b503a93b563ea0dc938bb511ea357eb15d9d
Kacper Marszelewski <116608850+Coosper@users.noreply.github.com>
2025-06-17 16:28:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Purpose: Custom JavaScript for the project.
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        'custom': ['CustomFont', 'sans-serif'],
+                        'brandon': ['BrandonFont', 'sans-serif']
+                    },
+                    colors: {
+                        'custom-yellow': '#e8c006',
+                        'custom-red': '#c91f62',
+                        'custom-green': '#76b729',
+                        'custom-purple': '#7c4b7c',
+                        'menu-background': '#f7eedf',
+                        'step1-border': '#e50067',
+                        'step1-background': '#f7d5d3',
+                        'step2-border': '#954091',
+                        'step2-background': '#EDDCD5',
+                        'step3-border': '#12999E',
+                        'step3-background': '#E6E8DB',
+                        'other-border': '#77B72B',
+                        'other-background': '#E9E8CC',
+                        'addon-border': '#ea6506',
+                        'addon-background': '#f6e0c8',
+                    }
+                }
+            }
+        }
